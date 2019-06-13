c952f901fde2d86b675c5bac07a16dad74723d97
keyana <2086765@talnet.nl>
2019-06-13 14:57:12 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+    include '../boot.php'; // laadt alle benodigdheden in
+    $title = 'Home';
+    include "../partials/head.php";
+?>
+
+<?php include "../partials/menu.php"; ?>
+
+<div class="container">
+    Home
+</div>
+
+<?php include "../partials/footer.php"; ?>
---
+++ public/shop/index.php
@@ -0,0 +1,15 @@
+<?php
+    include '../../boot.php'; // laadt alle benodigdheden in
+    $query = 'SELECT * FROM products WHERE slug = :slug';
+    $db = new DB;
+    $product = $db->find($query, ['slug' => $_GET['slug']]);
+    $title = $product['title'];
+    include "../../partials/head.php";
+    include "../../partials/menu.php"; ?>
+
+    <div class="container">
+        <img src="../img/<?= $product['image']; ?>">
+        <h1><?= $product['title']; ?></h1>
+    </div>
+
+<?php include "../../partials/footer.php"; ?>
